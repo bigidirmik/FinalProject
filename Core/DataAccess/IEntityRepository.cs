494581213aaf57b11f868bae97b2cf1a49664553
Bilal Dırmık <bigi_s@hotmail.com>
2021-02-12 02:44:22 +0300

    03.02.2021 Ders Sonu

--- Core/DataAccess/IEntityRepository.cs
+++ Core/DataAccess/IEntityRepository.cs
@@ -1,10 +1,10 @@
-﻿using Entities.Abstract;
+﻿using Core.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Text;
 
-namespace DataAccess.Abstract
+namespace Core.DataAccess
 {
     // Generic Constraint: Yalnızca Entities içindeki Concrete elemanları T'yi kullanabilsin istiyoruz.
     // where ile başlıyoruz
