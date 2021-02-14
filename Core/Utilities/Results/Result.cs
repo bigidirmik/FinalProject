1ed09f52f5b041cb11c5ad1fa1b082d877885f30
Bilal Dırmık <bigi_s@hotmail.com>
2021-02-14 18:05:01 +0300

    10 Şubat İlk Ders Sonu

---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+        public Result(bool success, string message) : this(success) // tek parametre yollandığında aşağıdaki ctor içindeki success'i çalıştır demek.
+        {
+            Message = message;
+        }
+
+        // ctor Over Loading
+        public Result(bool success) // IProductService içinde Add metodu artık sadece Success bilgisi (true) verebilir. Message yok.
+        {
+            Success = success;
+        }
+
+        public bool Success { get; }
+
+        public string Message { get; }
+    }
+}
